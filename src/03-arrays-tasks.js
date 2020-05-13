09ee7e573c227306bea4c11ee008a9034a84a9ad
Olya Kostenok <ola.kostenok@gmail.com>
2020-05-14 01:34:07 +0300

    feat: added solution for 5 exercises

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -177,6 +177,7 @@ function getStringsLength(arr) {
  *    [ 1, 3, 4, 5 ], 2, 1  => [ 1, 2, 3, 4, 5 ]
  *    [ 1, 'b', 'c'], 0, 'x'  => [ 'x', 1, 'b', 'c' ]
  */
+// ! НЕ РЕШИЛА
 function insertItem(/* arr, item, index */) {
   throw new Error('Not implemented');
 }
@@ -215,8 +216,14 @@ function getHead(arr, n) {
  *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  arr.map(() => {
+    if (n < arr.length) {
+      arr.shift();
+    }
+    return arr;
+  });
+  return arr;
 }
 
 
@@ -240,6 +247,7 @@ function getTail(/* arr, n */) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
+// ! НЕ РЕШИЛА
 function toCsvText(/* arr */) {
   throw new Error('Not implemented');
 }
@@ -255,8 +263,10 @@ function toCsvText(/* arr */) {
  *   [ 0, 1, 2, 3, 4, 5 ] => [ 0, 1, 4, 9, 16, 25 ]
  *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
  */
-function toArrayOfSquares(/* arr */) {
-  throw new Error('Not implemented');
+function toArrayOfSquares(arr) {
+  let result = [];
+  result = arr.map((elem) => elem * elem);
+  return result;
 }
 
 
@@ -274,6 +284,7 @@ function toArrayOfSquares(/* arr */) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
+// ! НЕ РЕШИЛА
 function getMovingSum(/* arr */) {
   throw new Error('Not implemented');
 }
@@ -289,6 +300,7 @@ function getMovingSum(/* arr */) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
+// ! НЕ РЕШИЛА
 function getSecondItems(/* arr */) {
   throw new Error('Not implemented');
 }
@@ -308,6 +320,7 @@ function getSecondItems(/* arr */) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
+// ! НЕ РЕШИЛА
 function propagateItemsByPositionIndex(/* arr */) {
   throw new Error('Not implemented');
 }
@@ -326,8 +339,8 @@ function propagateItemsByPositionIndex(/* arr */) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  return arr.reverse().slice(0, 3);
 }
 
 
@@ -344,8 +357,15 @@ function get3TopItems(/* arr */) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  let count = 0;
+  arr.map((elem) => {
+    if (typeof (elem) === 'number' && elem > 0) {
+      count += 1;
+    }
+    return count;
+  });
+  return count;
 }
 
 /**
@@ -377,8 +397,13 @@ function sortDigitNamesByNumericOrder(/* arr */) {
  *   [ -1, 1, -1, 1 ]      => 0
  *   [ 1, 10, 100, 1000 ]  => 1111
  */
-function getItemsSum(/* arr */) {
-  throw new Error('Not implemented');
+function getItemsSum(arr) {
+  let count = 0;
+  arr.map((elem) => {
+    count += elem;
+    return count;
+  });
+  return count;
 }
 
 /**
